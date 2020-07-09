e26bd7cde41c9744a769129e0a78ac3f57e3f756
Rosa <rosaislp@gmail.com>
2020-07-09 16:45:04 -0500

    run getCharactersAction function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,18 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'; 
 import userReducer from './userDuck';
-import charactersReducer from './charactersDuck';
+import charactersReducer, { getCharactersActions } from './charactersDuck';
 
 let rootReducer = combineReducers({
   user: userReducer,
   characters: charactersReducer,
 });
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function generateStore(){
   let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
-  return store;
+  // Consiguiendo los personajes por primera vez
+  getCharactersActions()(store.dispatch, store.getState);
+  return store
 };
