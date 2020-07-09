163f400c343ae9c5ce4d43ee951b1215abda7989
Rosa <rosaislp@gmail.com>
2020-07-08 20:27:21 -0500

    wrapper with provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import generateStore from './redux/store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import 'font-awesome/css/font-awesome.css'
 
-let WithRouter = () => <BrowserRouter><App /></BrowserRouter>
+let store = generateStore();
 
-ReactDOM.render(<WithRouter />, document.getElementById('root'));
+let WithRouter = () => <BrowserRouter><App /></BrowserRouter>
+let WithStore = () => <Provider store={store}><WithRouter/></Provider>
+
+ReactDOM.render(<WithStore />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
